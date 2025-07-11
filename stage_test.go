7a04d9a9e7885397aff47f088d93eedb52917f06
Yury Rash <yrashetska@gmail.com>
2025-07-11 10:05:06 +0300

    Add base stage tests

--- stage_test.go
+++ stage_test.go
@@ -1,8 +1,157 @@
 package pipeline
 
-import "testing"
+import (
+	"context"
+	"errors"
+	"strconv"
+	"sync"
+	"testing"
 
-func TestBaseStageInit(t *testing.T) {
-	s := baseStage[bool, int]{}
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestBaseStage_SimpleTransformation_Success(t *testing.T) {
+	s, sc := newBaseStage(func(ctx context.Context, i int) (string, error) {
+		return strconv.Itoa(i), nil
+	})
 	s.init()
+	res, err := runData(s, sc, noErr, 1, 2, 3)
+	require.NoError(t, err)
+	assert.Equal(t, []string{"1", "2", "3"}, res)
+	assert.NoError(t, s.verifyClosedChannel())
+}
+
+func TestBaseStage_SimpleTransformation_ErrorOnStage(t *testing.T) {
+	retErr := errors.New("error on stage")
+	s, sc := newBaseStage(func(ctx context.Context, i int) (string, error) {
+		if i == 2 {
+			return "", retErr
+		}
+		return strconv.Itoa(i), nil
+	})
+	s.init()
+	res, err := runData(s, sc, noErr, 1, 2, 3)
+	require.Error(t, err)
+	assert.ErrorIs(t, err, retErr)
+	assert.Equal(t, []string{"1"}, res)
+	assert.NoError(t, s.verifyClosedChannel())
+}
+
+func TestBaseStage_SimpleTransformation_ErrorOnPrevStage(t *testing.T) {
+	retErr := errors.New("error on prev stage")
+	s, sc := newBaseStage(func(ctx context.Context, i int) (string, error) {
+		return strconv.Itoa(i), nil
+	})
+	s.init()
+	res, err := runData(s, sc, func(v int) error {
+		if v == 2 {
+			return retErr
+		}
+		return nil
+	}, 1, 2, 3)
+	require.Error(t, err)
+	assert.ErrorIs(t, err, retErr)
+	assert.Equal(t, []string{"1"}, res)
+	assert.NoError(t, s.verifyClosedChannel())
+}
+
+func TestBaseStage_SimpleTransformation_NextStageIsClosed(t *testing.T) {
+	s, sc := newBaseStage(func(ctx context.Context, i int) (string, error) {
+		return strconv.Itoa(i), nil
+	})
+	s.init()
+	s.cancelFn()
+	res, err := runData(s, sc, noErr, 1, 2, 3)
+	require.NoError(t, err)
+	assert.Equal(t, []string{}, res)
+	assert.NoError(t, s.verifyClosedChannel())
+}
+
+func noErr[T any](T) error { return nil }
+func runData[I, O any](s *baseStage[I, O], sc stageChannels[I, O], errFn func(I) error, values ...I) ([]O, error) {
+	result := make([]O, 0, len(values))
+
+	var wg sync.WaitGroup
+
+	runStage := func() {
+		defer wg.Done()
+		s.run()
+	}
+	pushValues := func() {
+		defer wg.Done()
+		defer func() {
+			close(sc.in)
+			close(sc.inErr)
+		}()
+		index := 0
+		for {
+			select {
+			case <-s.ctx.Done():
+				return
+			default:
+				if index == len(values) {
+					return
+				}
+				v := values[index]
+				index++
+				if err := errFn(v); err != nil {
+					sc.inErr <- err
+					return
+				}
+				sc.in <- v
+			}
+		}
+	}
+	collectValues := func() {
+		defer wg.Done()
+		for v := range sc.out {
+			result = append(result, v)
+		}
+	}
+	var retErr error
+	collectErrors := func() {
+		defer wg.Done()
+		for err := range sc.outErr {
+			retErr = err
+		}
+	}
+
+	wg.Add(4)
+	go runStage()
+	go pushValues()
+	go collectValues()
+	go collectErrors()
+	wg.Wait()
+
+	return result, retErr
+}
+
+type stageChannels[I, O any] struct {
+	inErr  chan error
+	outErr chan error
+	in     chan I
+	out    chan O
+}
+
+func newBaseStage[I, O any](fn transformFn[I, O]) (*baseStage[I, O], stageChannels[I, O]) {
+	ctx, cf := context.WithCancel(context.Background())
+	sc := stageChannels[I, O]{
+		in:     make(chan I),
+		out:    make(chan O),
+		inErr:  make(chan error),
+		outErr: make(chan error),
+	}
+	return &baseStage[I, O]{
+		ctx:      ctx,
+		cancelFn: cf,
+		name:     "new base stage",
+
+		in:     sc.in,
+		out:    sc.out,
+		inErr:  sc.inErr,
+		outErr: sc.outErr,
+
+		transformationFn: fn,
+	}, sc
 }
